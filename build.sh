5395a76bea6db2c82d9a6a2b6db3d5e36eb68bbc
thanakarn <thanakarn@artisan.co.th>
2020-08-14 16:56:45 +0700

    :tada: initail
    # Conflicts:
    #	README.md

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+pushd vue-app
+yarn install
+yarn build
+popd
+
+# pushd react-app
+# yarn install
+# yarn build
+# popd
+
+
+# docker build -t nazt/webapp-starter .
+# docker push nazt/webapp-starter
+# docker run --rm -it -p4000:4000 --init nazt/webapp-starter
+# docker run --rm -it -p4000:4000 nazt/webapp-starter
